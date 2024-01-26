de827cc305a65e816a13fde8b4a5545781c469e7
PUMCZACZ <pawel.cebernik02@gmail.com>
2024-01-26 19:43:07 +0100

    2024-01-26

---
+++ app/Domains/Order/Requests/ApiOrderRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domains\Order\Requests;
+
+use App\Domains\Order\Enums\OrderTypeEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
+
+class OrderRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'client_id' => ['required','integer','exists:clients,id'],
+            'order_name' => ['required','string','max:255'],
+            'order_type' => ['required', OrderTypeEnum::toValidationRule()],
+            'quantity' => ['required','numeric','min:0.01'],
+            'price' => ['required','numeric','min:0.01'],
+            'deadline' => ['required','date'],
+            'unit_id' => ['required','exists:units,id']
+        ];
+    }
+}
