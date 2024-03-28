84883593e40e980bc7818bf2fca0730d876a51f9
Marcin - murzynek <marcinceber@gmail.com>
2024-03-28 22:13:12 +0100

    Make a Order view with payment and product relase views

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,63 +1,156 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Podgląd Zamówienia') . ': ' . $order->order_name }}
-        </h2>
-    </x-slot>
+@extends('layouts.master')
 
-    <div>
-        <button class="text-white">
-            <a href="{{ route('payment.create', $order) }}">Dodaj Płatność</a>
-        </button>
+@section('title', 'Podgląd zamówienia')
+
+@push('page-css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+@endpush
+
+@section('content')
+
+{{-- Karta zamówienia --}}
+<div class="container">
+    <div class="card card shadow-lg p-3 mb-3 bg-body rounded">
+    <div class="card-header shadow p-3 mb-2 bg-body rounded">
+        <h4>
+            <i class="bi bi-bar-chart-steps text-danger" style="font-size: 2rem;"> </i>
+            Zamówienie: 
+        </h4>
     </div>
-    <div class="py-12">
-        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="client_id">Klient</label>
-                            <input class="border-gray-200 rounded-md text-black" value="{{ $order->client->fullName() }}" disabled/>
-                        </div>
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="order_name">Nazwa Zamówienia</label>
-                            <input class="border-gray-200 rounded-md text-black" value="{{ $order->order_name}}" disabled/>
-                        </div>
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="order_type">Typ Zamówienia</label>
-                            <input class="border-gray-200 rounded-md text-black" value="{{ $order->order_type->translate() }}" disabled/>
-                        </div>
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="quantity">Ilość</label>
-                            <input type="number" step="0.01" class="border-gray-200 rounded-md text-black" value="{{ $order->quantity }}" disabled/>
-                        </div>
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="price">Cena</label>
-                            <input type="number" step="0.01" class="border-gray-200 rounded-md text-black" value="{{ $order->price }}" disabled/>
-                        </div>
-                        <div class="flex flex-col mb-4">
-                            <label class="mb-1" for="last_name">Szacowany Termin Realizacji</label>
-                            <input type="date" class="border-gray-200 rounded-md text-black" value="{{ \Carbon\Carbon::parse($order->deadline)->format('Y-m-d') }}" disabled/>
-                        </div>
-                    <div class="flex flex-col mb-4">
-                        <label class="mb-1" for="delivery_method">Sposób Dostawy</label>
-                        <input name="delivery_method" value="{{ $order->delivery_metohd?->translate() }}" disabled>
-                    </div>
-                    <div class="flex flex-col mb-4">
-                        <label class="mb-1" for="last_name">Termin Realizacji Zamówienia</label>
-                        <input class="border-gray-200 rounded-md text-black" value="{{ $order->realised_at ?? 'Nie zrealizowano' }}" disabled/>
-                    </div>
-                </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="client_id">
+                    <i class="bi bi-person"></i><span>Klient</span> 
+                </label>
+                <input class="form-control" value="{{ $order->client->fullName() }}" disabled/>
+                <button class="input-group-text">
+{{-- umieszczenie linku do podglądu danych klienta --}}
+                    <a href="">
+                        <i class="bi bi-info-circle-fill"></i>
+                    </a>
+                </button>
             </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="order_name">
+                    <i class="bi bi-card-text"></i> <span>Nazwa Zamówienia</span>
+                </label>
+                <input class="form-control" value="{{ $order->order_name}}" disabled/>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="order_type">
+                    Typ Zamówienia
+                </label>
+                <input class="form-control" value="{{ $order->order_type->translate() }}" disabled/>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="quantity">
+                    Ilość
+                </label>
+                <input type="number" step="0.01" class="form-control" value="{{ $order->quantity }}" disabled/>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="price">
+                    Cena
+                </label>
+                <input type="number" step="0.01" class="form-control" value="{{ $order->price }}" disabled/>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="last_name">
+                    Szacowany termin realizacji
+                </label>
+                <input type="date" class="form-control" value="{{ \Carbon\Carbon::parse($order->deadline)->format('Y-m-d') }}" disabled/>
+            </div>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="delivery_method">
+                Sposób dostawy
+            </label>
+            <input class="form-control" name="delivery_method" value="{{ $order->delivery_metohd?->translate() }}" disabled>
+        </div>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="last_name">
+                Termin realizcji zamówienia
+            </label>
+            <input class="form-control" value="{{ $order->realised_at ?? 'Nie zrealizowano' }}" disabled/>
         </div>
     </div>
-    <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <h1 class="text-lg font-bold">Dane Klienta</h1>
-                <div></div>
-            </div>
+    
+{{-- Płatności --}}
+    <div class="card card shadow-lg p-3 mb-3 bg-body rounded">
+        <div class="card-header shadow p-3 mb-2 bg-body rounded">
+            <h4>
+                <i class="bi bi-wallet-fill text-danger" style="font-size: 2rem;"> </i>
+                Płatności do zamówienia: 
+            </h4>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <td scope="col">L.p</td>
+                        <td scope="col" >Kwota</td>
+                        <td scope="col">Termin wpłaty</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>100 zł</td>
+                        <td>12.11.2019</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-3 text-end">
+            <p class="card-text">Suma płatności: <span>100 zł</span></p>
+            <p class="card-text">Kwota zamówienia: <span>{{ $order->price }} zł</span></p>
+            <p class="card-text {{-- Jeśli pozostało do zapłaty >0 text-danger jeśli = 0 text-success  --}}">Pozostało do zapłaty: <span>1000 zł</span></p>
+        </div>
+        <div class="mb-3 text-end">
+            <a class="btn btn-warning" href="{{ route('payment.create', $order) }}" role="button">Dodaj płatność częściową</a>
+            <a class="btn btn-success" href="{{ route('payment.create', $order) }}" role="button">Dodaj płatność całkowitą</a> 
         </div>
     </div>
-</x-app-layout>
+    </div>
+
+    {{-- Wydania --}}
+
+    <div class="card card shadow-lg p-3 mb-3 bg-body rounded">
+        <div class="card-header shadow p-3 mb-2 bg-body rounded">
+            <h4>
+                <i class="bi bi-box-arrow-right text-danger" style="font-size: 2rem;"> </i>
+                Wydania do zamówienia: 
+            </h4>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <td scope="col">L.p</td>
+                        <td scope="col" >Ilość</td>
+                        <td scope="col">Termin wydania</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>100 kg</td>
+                        <td>12.11.2019</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mb-3 text-end">
+            <p class="card-text">Suma wydań: <span>100 kg</span></p>
+            <p class="card-text">Ilość z zamówienia: <span>{{ $order->price }} zł</span></p>
+            <p class="card-text {{-- Jeśli pozostało do zapłaty >0 text-danger jeśli = 0 text-success  --}}">Pozostało do wydania: <span>1000 zł</span></p>
+        </div>
+        <div class="mb-3 text-end">
+            <a class="btn btn-warning" href="{{ route('payment.create', $order) }}" role="button">Dodaj wydanie częściowe</a>
+            <a class="btn btn-success" href="{{ route('payment.create', $order) }}" role="button">Dodaj wydanie całkowite</a> 
+        </div>
+    </div>
+    </div>
+   
+    </div>
+@endsection
 
 
