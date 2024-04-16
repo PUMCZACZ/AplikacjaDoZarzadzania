6e9b4f9eb6080e1c29bf49184d7ea3fbc6df4212
PUMCZACZ <pawel@pawel-VirtualBox>
2024-04-16 21:16:24 +0200

    fix dashboard view

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,102 +8,41 @@
 
 @section('content')
     <div class="container">
-            <div class="row mt-4">
-                    <div class="col-sm-12">
-                            <div class="card shadow-lg p-3 mb-3 bg-body rounded">
+        <div class="row mt-4">
+            <div class="col-sm-12">
+                <div class="card shadow-lg p-3 mb-3 bg-body rounded">
+                    <div class="card-header shadow p-3 mb-2 bg-body rounded">
+                        Aktualny miesiąc
+                    </div>
+                    <div class="row mt-4 mb-4 px-2">
+                        <div class="col-sm-4">
+                            <div class="card mb-2">
                                 <div class="card-header shadow p-3 mb-2 bg-body rounded">
                                     Aktualny miesiąc
                                 </div>
-                                <div class="row mt-4 mb-4 px-2">
-                                    <div class="col-sm-4">
-                                        <div class="card mb-2">
-                                            <div class="card-header shadow p-3 mb-2 bg-body rounded">
-                                            Aktualny miesiąc
-                                            </div>
-                                            <div class="card-body mb-2">
-                                                {{ $materialDemand }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="card mb-2">
-                                            <div class="card-header shadow p-3 mb-2 bg-body rounded">
-                                            Następny miesiąc
-                                            </div>
-                                            <div class="card-body mb-2">
-                                            {{ $nextMonthDemand }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="card mb-2">
-                                            <div class="card-header shadow p-3 mb-2 bg-body rounded">
-                                            Za 2 miesiące
-                                            </div>
-                                            <div class="card-body mb-2">
-                                            {{ $nextTwoMonthDemand }}
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="card mt-2 shadow-lg p-3 mb-4 bg-body rounded">
-                        <div class="card-header shadow p-3 mb-4 bg-body rounded">
-                            <h1><i class="bi bi-truck pd-1 text-danger" style="font-size: 2rem;"> </i>Tabela wywozów / odbiorów</h1>
-                        </div>
-                        <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="dates-filters-container">
-                                    <label>
-                                        Od
-                                        <input id="date_from" type="date" name="date_from" class=""
-                                               value="{{ \Illuminate\Support\Carbon::now()->toDateString() }}" />
-                                    </label>
-                                    <label>
-                                        Do
-                                        <input id="date_to" type="date" name="date_to" value="{{ \Illuminate\Support\Carbon::now()->toDateString() }}">
-                                    </label>
+                                <div class="card-body mb-2">
+                                    {{ $materialDemand }}
                                 </div>
                             </div>
-                            <div class="col-sm-6">
-                                <p class="mb-2">Kateogria</p>
-                                <select id="delivery-type" class="form-select shadow mb-5">
-                                <option value="all">Wszystkie</option>
-                                @foreach(\App\Domains\Order\Enums\OrderDeliveryMethodEnum::cases() as $orderDeliveryMethod)
-                                <option value="{{ $orderDeliveryMethod->value }}">
-                                    {{ $orderDeliveryMethod->translate() }}
-                                </option>
-                                @endforeach
-                                </select>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card mb-2">
+                                <div class="card-header shadow p-3 mb-2 bg-body rounded">
+                                    Następny miesiąc
+                                </div>
+                                <div class="card-body mb-2">
+                                    {{ $nextMonthDemand }}
+                                </div>
                             </div>
                         </div>
-                        <div class="row p-2">
-
-                            <div class="table-responsive">
-                                <table id="datatable" class="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col" class="">{{ __('Id') }}</th>
-                                            <th scope="col" class="">{{ __('Klient') }}</th>
-                                            <th scope="col" class="">{{ __('Nazwa Zamówienia') }}</th>
-                                            <th scope="col" class="">{{ __('Typ Zamówienia') }}</th>
-                                            <th scope="col" class="">{{ __('Ilość') }}</th>
-                                            <th scope="col" class="">{{ __('J.M') }}</th>
-                                            <th scope="col" class="">{{ __('Cena') }}</th>
-                                            <th scope="col" class="">{{ __('Termin Realizacji') }}</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
-                                    </tbody>
-                                </table>
+                        <div class="col-sm-4">
+                            <div class="card mb-2">
+                                <div class="card-header shadow p-3 mb-2 bg-body rounded">
+                                    Za 2 miesiące
+                                </div>
+                                <div class="card-body mb-2">
+                                    {{ $nextTwoMonthDemand }}
+                                </div>
                             </div>
                         </div>
 
@@ -113,7 +52,6 @@
             </div>
         </div>
 
-
         <div class="card mt-2 shadow-lg p-3 mb-4 bg-body rounded">
             <div class="card-header shadow p-3 mb-4 bg-body rounded">
                 <h1><i class="bi bi-truck pd-1 text-danger" style="font-size: 2rem;"> </i>Tabela wywozów / odbiorów</h1>
@@ -121,13 +59,18 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-6">
-                        <p class="mb-2">Zakres dni</p>
-                        <select id="day-filter" class="form-select shadow mb-3">
-                            <option value="1">{{ __('Dzisiaj') }}</option>
-                            <option value="3">{{ __('3 dni') }}</option>
-                            <option value="7">{{ __('7 dni') }}</option>
-                            <option value="all">{{ __('Wszystkie') }}</option>
-                        </select>
+                        <div class="dates-filters-container">
+                            <label>
+                                Od
+                                <input id="date_from" type="date" name="date_from" class=""
+                                       value="{{ \Illuminate\Support\Carbon::now()->toDateString() }}"/>
+                            </label>
+                            <label>
+                                Do
+                                <input id="date_to" type="date" name="date_to"
+                                       value="{{ \Illuminate\Support\Carbon::now()->toDateString() }}">
+                            </label>
+                        </div>
                     </div>
                     <div class="col-sm-6">
                         <p class="mb-2">Kateogria</p>
@@ -172,7 +115,8 @@
             <div class="card-body">
                 <div class="row justify-content-center">
                     <div class="col-6 text-center">
-                        <p class="text-center inline" style="font-size:4rem;" id="sumValue"></p> <p class="inline" style="font-size:2rem;">zł</p>
+                        <p class="text-center inline" style="font-size:4rem;" id="sumValue"></p>
+                        <p class="inline" style="font-size:2rem;">zł</p>
                     </div>
                 </div>
             </div>
